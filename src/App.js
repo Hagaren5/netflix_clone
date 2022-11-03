0a5e6e00598efce792c8b2f57c3f6de6fc5821d9
Hagaren5 <oriolsm93@gmail.com>
2022-11-03 10:13:36 +0100

    Added profileScreen and fixed refresh page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import './App.css'
 import HomeScreen from './pages/HomeScreen'
 import LoginScreen from './pages/LoginScreen'
+import ProfileScreen from './pages/ProfileScreen'
 import { BrowserRouter as Routes, Route } from 'react-router-dom'
 import { auth } from './firebase'
 import { useDispatch, useSelector } from 'react-redux'
@@ -24,9 +25,8 @@ function App() {
         dispatch(logout)
       }
     })
-
     return unsubscribe
-  }, [])
+  }, [dispatch])
 
   return (
     <div className="app">
@@ -34,9 +34,14 @@ function App() {
         {!user ? (
           <LoginScreen />
         ) : (
-          <Route exact path="/">
-            <HomeScreen />
-          </Route>
+          <Routes>
+            <Route path="/profile">
+              <ProfileScreen />
+            </Route>
+            <Route path="/">
+              <HomeScreen />
+            </Route>
+          </Routes>
         )}
       </Routes>
     </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,21 @@
 import React, { useState, useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
 
 import './Nav.css'
 
 function Nav() {
   const [show, handleShow] = useState(false)
+  const history = useHistory()
+
+  function handleClickProfile() {
+    history.push("/profile")
+    history.go()
+  }
+
+  function handleClickHome() {
+    history.push("/")
+    history.go()
+  }
 
   const transitionNavBar = () => {
     if (window.scrollY > 100) {
@@ -21,9 +33,10 @@ function Nav() {
   return (
     <div className={`nav ${show && 'nav__black'}`}>
       <div className="nav__contents">
-        <img className="nav__logo" src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="" />
+        <img onClick={handleClickHome} className="nav__logo" src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="" />
 
         <img
+          onClick={handleClickProfile} 
           className="nav__avatar"
           src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/84c20033850498.56ba69ac290ea.png"
           alt=""
